d8389556656deda898af2ceaafe094d4f81de6f0
Grayson Shogan <graysontoddshogan@gmail.com>
2023-03-16 17:09:50 -0600

    Working on Likes and Comments

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
     <div className="App">
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Posts posts={posts} likePost={likePost} />
+      <Posts posts={posts} likePost={likePost} key={posts.id} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
